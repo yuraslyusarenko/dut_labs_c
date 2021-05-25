a457bc77050031de349315fcea699803032261c3
yuraslyusarenko <56907658+yuraslyusarenko@users.noreply.github.com>
2021-05-25 17:14:52 +0300

    Add files via upload

---
+++ lab1_3_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+int main()
+{
+int three_hours_in_sec = 10800; // 3*60*60
+
+int three_hours = 3*60;// This is a variable to hold the value of 3 minutes in seconds
+
+int six_hours = 5*60;// This is a variable to hold the value of 6 minutes in seconds
+
+float number_pi = 3.1415926; //This is the value of pi
+
+std::cout<<"the result: "<< three_hours_in_sec;
+}
